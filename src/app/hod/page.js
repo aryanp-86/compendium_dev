6fe068afa8d0603bc91b664bd8a3272b30fae4b7
aryanp86 <aryan.pokharkar21@vit.edu>
2024-04-14 20:44:23 +0530

    Hod page complete

--- src/app/hod/page.js
+++ src/app/hod/page.js
@@ -7,6 +7,7 @@ import SvgComponent5 from "../Components/SvgComponent5";
 import Link from "next/link";
 import { LampDemo } from "../ui/lamp";
 import { CardHover } from "../Components/CardHover";
+import Footer from "../Components/Footer";
 const navigation = [
     { name: "Home", href: "/" },
     { name: "From HOD's Desk", href: "/hod" },
@@ -25,10 +26,9 @@ const Hod = () => {
     }, []);
     return (
         // <img src="https://i.ibb.co/47yyLMS/Untitled.jpg" alt="Untitled" border="0"></img>
-        <div className="hod w-full">
+        <div className="hod w-full overflow-y-clip">
             <header
-                className={`inset-x-0 h-20 text-white bg-black top-0 z-50 nav-link ${scroll ? "sticky" : ""
-                    }`}
+                className="inset-x-0 h-20 text-white bg-black top-0 z-50 nav-link"
             >
                 <nav
                     className="flex items-center justify-between p-6 lg:px-8"
@@ -120,39 +120,15 @@ const Hod = () => {
                     </Dialog.Panel>
                 </Dialog>
             </header>
-            {/* <section className="overflow-hidden mt-16 ">
-                <div className="mx-auto px-4 py-2 lg:flex lg:items-center lg:flex-col  rounded-2xl">
-                    <div className="mx-auto text-center overflow-y-clip">
-                        <h1 className="bebas-font tracking-widest text-5xl font-extrabold lg:text-8xl mt-8">
-                            From HOD's Desk
-                        </h1>
-                    </div>
-                </div>
-            </section> */}
-            <div className="grid grid-cols-3">
+            <div className="grid grid-cols-3 h-screen">
                 <div className="h-3/4 col-span-1 flex items-center justify-center bg-slate-950">
                     <CardHover />
                 </div>
-                <div className="col-span-2">
+                <div className="col-span-2 -z-20">
                     <LampDemo />
                 </div>
             </div>
-            {/* 
-            <div className="flex flex-col max-w-lg mt-12 items-center justify-center">
-                <a href="#" className="block">
-                    <img
-                        alt=""
-                        src="https://i.ibb.co/47yyLMS/Untitled.jpg"
-                        className="h-92 w-full object-fit border1 shadow-xl rounded-xl "
-                    />
-
-                    <h3 className="mt-4 text-lg font-bold text-gray-900 bebas-font tracking-widest lg:text-2xl text-center">Prof. (Dr.) Sandip R. Shinde</h3>
-
-                    <p className="mt-2 max-w-lg mb-4 text-base font-medium text-gray-700">
-                        Professor & Head, Department of Computer Engineering
-                    </p>
-                </a>
-            </div> */}
+            <Footer />
         </div>
     );
 };
