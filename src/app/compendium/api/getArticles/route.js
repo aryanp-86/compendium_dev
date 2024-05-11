b668e62a0c2e7b3517a5dcc6952323b3e10226e5
aryanp86 <aryan.pokharkar21@vit.edu>
2024-05-11 19:57:33 +0530

    Update route.js

--- src/app/compendium/api/getArticles/route.js
+++ src/app/compendium/api/getArticles/route.js
@@ -2,9 +2,11 @@
 import { connectMongoDB } from "@/utils/mongodb";
 import { NextResponse } from "next/server";
 import Article from "@/models/article";
+import { unstable_noStore } from "next/cache";
 
 
 export async function GET(req) {
+    unstable_noStore();
     try {
         await connectMongoDB();
         const articles = await Article.find({});
